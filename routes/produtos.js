7281f61a931277192c4bc35a781e5693c8f8fad1
Zaqueu Ricardo <zaqueu.ricardo@luminae.com.br>
2020-11-23 22:52:03 -0200

    Update routes pedidos file

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) =>{
     res.status(200).send({
         mensagem: 'Usando o GET dentro da rota de produtos.',
     });
 });
 
+// INSERE UM PRODUTO 
 router.post('/', (req, res, next) =>{
     res.status(201).send({
         mensagem: "Usando o POST dentro da rota de produtos.",
     });
 });
 
-// Passando parametro na rota
+// RETORNA OS DADOS DE UM PRODUTO
+    // Passando parametro na rota
 router.get('/:id_produto', (req, res, next) =>{
     const id = req.params.id_produto;
 
@@ -28,6 +31,20 @@ router.get('/:id_produto', (req, res, next) =>{
             id: id,
         });
     }
+});
+
+// 
+router.patch('/', (req, res, next) =>{
+    res.status(201).send({
+        mensagem: "Usando o PATCH dentro da rota de produtos.",
+    });
+});
+
+// 
+router.delete('/', (req, res, next) =>{
+    res.status(201).send({
+        mensagem: "Usando o DELETE dentro da rota de produtos.",
+    });
 });
 
 module.exports = router;
